04083b31e5b018646cef8424c6e86c40f99d2c4f
Cristian Casallas <cristiancasallas998@gmail.com>
2023-02-15 10:51:31 -0500

    initial sidebase commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+import { setAbsoluteSqliteDatabaseUrlForPrisma } from './prisma/utils'
+
+setAbsoluteSqliteDatabaseUrlForPrisma()
+
+export default defineNuxtConfig({
+  runtimeConfig: {
+    version: '0.0.1'
+  },
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-svgo', '@huntersofbook/naive-ui-nuxt'],
+  extends: ['@sidebase/core'],
+  typescript: {
+    shim: false
+  }
+})
